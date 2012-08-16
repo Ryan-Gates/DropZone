25799099645800e94ae77ffea5c2286f849fae4b
Ryan-Gates <rpgates@suddenlink.net>
2012-08-16 11:25:44 -0500

    added basic functionality

--- DropZone.js
+++ DropZone.js
@@ -1,41 +1,86 @@
-var $dropZone
+
 function drop(e){
-	e.stopPropagation();
-	e.preventDefault();
-	alert("hello");
+    dragOver(e);
+
+    // fetch FileList object
+    var files = e.target.files || e.dataTransfer.files;
+
+    // process all File objects
+    for (var i = 0, f; f = files[i]; i++) {
+       
+        UploadFile(f);
+    }
+	
 }
 
 function dragOver(e){
 	e.stopPropagation();
 	e.preventDefault();
+	e.target.className = (e.type == "dragover" ? "hover" : "");
 }
 
 function dragExit(e){
 	e.stopPropagation();
 	e.preventDefault();
+	e.target.className = (e.type == "dragover" ? "hover" : "");
 }
 
 function dragEnter(e){
 	e.stopPropagation();
 	e.preventDefault();
+	e.target.className = (e.type == "dragover" ? "hover" : "");
 }
-function bindEvents(){
-	$dropZone.addEventListener("dragenter", dragEnter, false);
-	$dropZone.addEventListener("dragexit", dragExit, false);
-	$dropZone.addEventListener("dragover", dragOver, false);
-	$dropZone.addEventListener("drop", drop, false);
+
+function UploadFile(file) {
+    var data = $dropZone.data('options');
+
+    var xhr = new XMLHttpRequest();
+
+    if (xhr.upload && file.type == "image/jpeg" && file.size <= 1000000) {
+        xhr.open("POST", data.options.url, true);
+        xhr.setRequestHeader("X_FILENAME", file.name);
+        xhr.send(file);
+    }
+}
+
+function bindEvents(dropZone){
+	dropZone.bind("dragenter", dragEnter);
+	dropZone.bind("dragexit", dragExit);
+	dropZone.bind("dragover", dragOver);
+	dropZone.bind("drop", drop);
+}
+function unBindEvents(dropZone) {
+    dropZone.unbind("dragenter", dragEnter);
+    dropZone.unbind("dragexit", dragExit);
+    dropZone.unbind("dragover", dragOver);
+    dropZone.unbind("drop", drop);
 }
 (function( $ ) {
 
 var methods ={
 	//url to post data, override methods
-	$dropZone =$(this);
-	$dropZone.data('options',options);
-	init : function( options){
-		
+   
+	
+    init: function (options) {
+
+        if (options.url !== undefined) {
+            var $this = $(this);
+            $this.data('options', options);
+            bindEvents($this);
+        }
+        else {
+            $.error('The URL to upload was not defined');
+        }
 	},
 	destroy : function (){
-	
+	    
+	    var $this = $(this),
+             data = $this.data('options');
+	    unBindEvents($this);
+	    // Namespacing FTW
+	    $(window).unbind('.DropZone');
+	    data.tooltip.remove();
+	    $this.removeData('options');
 	}
 };
 
